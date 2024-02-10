24505a385dff770734fb065d88a544c4ac2820dd
Meow Roar <sareacct91@gmail.com>
2024-02-10 11:06:10 -0800

    explicit use of unique key in model association

--- models/index.js
+++ models/index.js
@@ -9,9 +9,12 @@ Product.belongsTo(Category);
 // Categories have many Products
 Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag });
+Product.belongsToMany(Tag, { through: ProductTag, uniqueKey: 'product_id' });
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag });
+Tag.belongsToMany(Product, { through: ProductTag, uniqueKey: 'tag_id' });
+
+// I am using underscored: true in the model of Product and Tag
+// So the default tagId and productId from the through table will coverted to snake_case to match the attribute in the model
 
 module.exports = {
   Product,
